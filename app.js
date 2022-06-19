dbab30c5abe024f718678f63e4529de0fcbc75c9
Nitzan Cohen <nitzancohen@hotmail.com>
2022-06-19 19:21:41 +0300

    Update limiter

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const app = express();
 
 const limiter = rateLimiter({
   windowMs: 10 * 60 * 1000, // 10 minutes.
-  max: 50, // Maximum of 100 requeset from each IP to the server.
+  max: 50, // Maximum of 50 requeset from each IP to the server.
 })
 
 const { PORT = 3000 } = process.env;
