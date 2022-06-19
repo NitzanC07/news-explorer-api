2182d2ff6ec46a4fa26424b9102a5abab8862403
Nitzan Cohen <69467531+NitzanC07@users.noreply.github.com>
2022-06-19 19:22:46 +0300

    Revert "Stage 1"

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const helmet = require('helmet');
-const rateLimiter = require('express-rate-limit');
 const cors = require('cors');
 const {
   errors,
@@ -18,12 +17,6 @@ const {
 require('dotenv').config();
 
 const app = express();
-
-const limiter = rateLimiter({
-  windowMs: 10 * 60 * 1000, // 10 minutes.
-  max: 50, // Maximum of 100 requeset from each IP to the server.
-})
-
 const { PORT = 3000 } = process.env;
 const usersRouter = require('./routes/users');
 const articlesRouter = require('./routes/articles');
@@ -37,17 +30,16 @@ const {
 
 const allowedOrigins = [
   'http://146.148.67.231:3000',
-  'http://localhost:3000',
   'https://nitzan-final-project.students.nomoreparties.sbs',
   'https://www.nitzan-final-project.students.nomoreparties.sbs',
   'https://api.nitzan-final-project.students.nomoreparties.sbs',
+  'http://localhost:3000',
 ];
 
 mongoose.connect('mongodb://localhost:27017/newsExplorer');
 
 app.use(helmet());
 app.use(bodyParser.json());
-app.use(limiter);
 app.use(cors());
 app.options(allowedOrigins, cors());
 
